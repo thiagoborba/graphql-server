85d038359f6fe39e90380caef34a7932f2c7ee57
Thiago Borba <thborba1@gmail.com>
2022-09-09 15:03:18 -0300

    feat: creates user model

--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -1,10 +1,26 @@
-import { Resolver, Query } from 'type-graphql'
+import { Resolver, Query, Mutation, Arg } from 'type-graphql'
+import { User } from '../models/User'
 
 @Resolver()
 export class UserResolver {
+  private data: User[] = []
 
-  @Query(() => String)
-  async hello() {
-    return 'hello word'
+  @Query(() => [User])
+  async user() {
+    return this.data
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg('name') name: string
+  ) {
+    const user =  {
+      id: '1',
+      name
+    }
+
+    this.data.push(user)
+
+    return user
   }
 }
